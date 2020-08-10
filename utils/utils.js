ff162556e9901fed6157bfe97a6366ad70ec6313
Zatnik <Zatnik@users.noreply.github.com>
2020-08-10 01:06:25 -0700

    Info, utils, misc stuff
    - Added changelog to info and made some minor edits
    - Made Zatnik-to-PNG more failsafe, and added view scale option
    - Added icon to pages
    - Added link to a new project, and made the project links open a new tab

--- utils/utils.js
+++ utils/utils.js
@@ -7,8 +7,11 @@ var pngInp = document.getElementById("pngInp"),
     zatnikInp = document.getElementById("zatnikInp"),
     zatnikConvertButton = document.getElementById("zatnikConvertButton"),
     zatnikViewButton = document.getElementById("zatnikViewButton"),
+    pngScaleInp = document.getElementById("pngScaleInp"),
+    zatnikErrorContainer = document.getElementById("zatnikErrorContainer"),
+    zatnikErrorElem = document.getElementById("zatnikErrorElem"),
     pngOutContainer = document.getElementById("pngOutContainer"),
-    pngOut = document.getElementById("pngOut");
+    pngOut = document.getElementById("pngOut"),
     pngViewContainer = document.getElementById("pngViewContainer"),
     pngView = document.getElementById("pngView"),
     commentsElem = document.getElementById("commentsElem"),
@@ -91,151 +94,269 @@ function hexByte(num) {
 
 
 
-zatnikConvertButton.onclick = () => convertZatnik(url => {
-	pngOut.href = url;
-	pngOutContainer.classList.remove("hidden");
+zatnikConvertButton.addEventListener("click", () => {
+	let c = convertZatnik();
+	if (c) {
+		pngOut.href = c[0];
+		pngOutContainer.classList.remove("hidden");
+	}
 });
 
-zatnikViewButton.onclick = () => convertZatnik((url, {comments}) => {
-	pngView.src = url;
-	if (comments.length) {
-		commentsList.innerHTML = "";
-		comments.forEach(c => {
-			var elem = document.createElement("div");
-			elem.textContent = c;
-			commentsList.appendChild(elem);
-		});
-		commentsElem.classList.remove("hidden");
-	} else commentsElem.classList.add("hidden");
-	pngViewContainer.classList.remove("hidden");
+let viewingImageWidth, viewingImageHeight;
+
+zatnikViewButton.addEventListener("click", () => {
+	let c = convertZatnik();
+	if (c) {
+		let [url, {width, height, comments}] = c;
+		viewingImageWidth = width;
+		viewingImageHeight = height;
+		pngView.src = url;
+		pngView.width = width * pngScaleInp.value;
+		pngView.height = height * pngScaleInp.value;
+		if (comments.length) {
+			commentsList.innerHTML = "";
+			comments.forEach(c => {
+				var elem = document.createElement("div");
+				elem.textContent = c;
+				commentsList.appendChild(elem);
+			});
+			commentsElem.classList.remove("hidden");
+		} else commentsElem.classList.add("hidden");
+		pngViewContainer.classList.remove("hidden");
+	}
+});
+
+pngScaleInp.addEventListener("change", () => {
+	pngView.width = viewingImageWidth * pngScaleInp.value;
+	pngView.height = viewingImageHeight * pngScaleInp.value;
 });
 
 function convertZatnik(callback) {
-	var canvas = document.createElement("canvas"),
-		ctx = canvas.getContext("2d"),
-		image = parseZatnik(zatnikInp.value),
-		data = zatnikToImageData(image);
-	
-	canvas.width = data.width;
-	canvas.height = data.height;
-	ctx.putImageData(data, 0, 0);
-	
-	callback(canvas.toDataURL(), image);
+	try {
+		zatnikErrorContainer.classList.add("hidden");
+		pngOutContainer.classList.add("hidden");
+		pngOut.href = "";
+		pngViewContainer.classList.add("hidden");
+		pngView.src = "";
+		
+		var canvas = document.createElement("canvas"),
+			ctx = canvas.getContext("2d"),
+			image = parseZatnik(zatnikInp.value),
+			data = zatnikToImageData(image);
+		
+		canvas.width = data.width;
+		canvas.height = data.height;
+		ctx.putImageData(data, 0, 0);
+		
+		return [canvas.toDataURL(), image];
+	} catch(e) {
+		console.log(e);
+		zatnikErrorElem.textContent = e;
+		zatnikErrorContainer.classList.remove("hidden");
+		return false;
+	}
 }
 
 
 function zatnikToImageData(image) {
 	if (typeof image === "string") image = parseZatnik(image);
 	
-	var data = new ImageData(image.width, image.height),
-		pixels = data.data;
-	
-	image.pixels.forEach(([r, g, b, a], i) => {
-		i *= 4;
-		pixels[i] = r;
-		pixels[i + 1] = g;
-		pixels[i + 2] = b;
-		pixels[i + 3] = a;
-	});
-	
-	return data;
+	return new ImageData(new Uint8ClampedArray(image.pixels), image.width, image.height);
 }
 
 function parseZatnik(code) {
-	code = code.toLowerCase();
+	code = code.toLowerCase().replace(/^\s+|\s+$/g, "");
 	
-	var sections = {},
+	let i = 0,
+		sections = {},
 		prevOrderedSection = -1;
-		
-	while (code) {
-		let section, name;
-		[code, section, name] = parseZatnikSection(code);
+	
+	while (i < code.length) {
+		let [name, ...sectionInfo] = parseSection();
 		if (name) {
 			if (multiSections.has(name)) {
-				(sections[name] || (sections[name] = [])).push(section);
-			} else sections[name] = section;
-		} else sections[++prevOrderedSection] = section;
+				(sections[name] || (sections[name] = [])).push(sectionInfo);
+			} else sections[name] = sectionInfo;
+		} else sections[++prevOrderedSection] = sectionInfo;
 	}
 	
-	var idLength = parseInt(sections[1][0]),
-		colorDefs = [],
-		alphas = [];
+	let section, offset, j;
 	
-	for (let i = 1; i < sections[1].length; i += idLength + 6) {
-		colorDefs.push([
-			sections[1].substring(i, i + idLength),
-			sections[1].substring(i + idLength, i + idLength + 6)
-		]);
+	if (!sections[0]) throw new Error("No unnamed sections found");
+	loadSection(0);
+	
+	let width = parseNumber(n => n > 0);
+	
+	checkEnd();
+	if (section[j] !== "*") throw new Error(`Expected asterisk at character ${j + 1 + offset}`);
+	j++;
+	
+	let height = parseNumber(n => n > 0);
+	
+	if (!sections[1]) throw new Error("Only one unnamed section found");
+	loadSection(1);
+	
+	let charsPerColor = parseNumber(n => n > 0, 1),
+		colorDefs = [];
+	
+	while (j < section.length) {
+		let code = parseString(charsPerColor);
+		if (section[j] === "=") j++;
+		let color = parseString(6, /[0-9a-f]/);
+		
+		colorDefs.push([code, color]);
 	}
+	
+	let alphaDefs = [];
 	
 	if (sections.alph) {
-		for (let i = 0; i < sections.alph.length; i += 2)
-			alphas.push(sections.alph.substring(i, i + 2));
+		loadSection("alph");
+		while (j < section.length) {
+			alphaDefs.push(parseNumber(n => 0 <= n && n <= 255, 2, "0123456789abcdef"));
+		}
 	}
 	
-	var [width, height] = sections[0].split("*").map(n => parseInt(n)),
-		colors = {},
-		pixels = [];
+	let colors = {};
 	
-	colorDefs.forEach(([id, color], i) => {
-		colors[id] = [
+	colorDefs.forEach(([code, color], i) => {
+		colors[code] = [
 			parseInt(color.substring(0, 2), 16),
 			parseInt(color.substring(2, 4), 16),
 			parseInt(color.substring(4, 6), 16),
-			alphas[i] ? parseInt(alphas[i], 16) : 255
+			alphaDefs[i] === undefined ? 255 : alphaDefs[i]
 		];
 	});
 	
-	for (let i = 0; i < sections[2].length; i += idLength)
-		pixels.push(colors[sections[2].substring(i, i + idLength)]);
+	if (!sections[2]) throw new Error("Only two unnamed sections found");
+	loadSection(2);
 	
-	return {
-		width, height, pixels,
-		comments: sections.comm ? sections.comm.map(s => parseZatnikText(s)) : []
-	}
-}
-
-function parseZatnikSection(code) {
-	var i = code[0] === "/" ? 1 : 0,
-		name;
+	let numPixelValues = width * height * 4,
+		pixels = [];
+	
+	while (j < section.length && pixels.length < numPixelValues) {
+		let startJ = j,
+			code = parseString(charsPerColor),
+			color = colors[code];
 		
-	if (code[i] === "/") {
-		name = code.substring(i + 1, i + 5);
-		i += 5;
+		if (!color) throw new Error(`Unknown color code ${JSON.stringify(code)} at character ${startJ + 1 + offset}`);
+		
+		pixels.push(...color);
 	}
 	
-	var nextSlash = code.indexOf("/", i),
-		sectionEnd = nextSlash === -1 ? code.length : nextSlash;
+	if (pixels.length < numPixelValues) throw new Error(`Image is missing ${(numPixelValues - pixels.length)/4} pixels`);
 	
-	return [code.substring(sectionEnd), code.substring(i, sectionEnd), name];
-}
-
-function parseZatnikText(code) {
-	var result = "";
-	for (let i = 0; i < code.length;) {
-		if (code[i] === "\\") {
-			let type = code[i + 1];
-			if (type === "u") {
-				let endIndex = code.indexOf(";", i);
-				result += String.fromCodePoint(parseInt(code.substring(i + 2, endIndex), 16));
-				i = endIndex + 1;
-			} else if (type === "c") {
-				result += code[i + 2].toUpperCase();
-				i += 3;
-			} else {
-				if (type === "l") result += "/";
-				else if (type === "s") result += " ";
-				else if (type === "t") result += "\t";
-				else if (type === "n") result += "\n";
-				else if (type === "r") result += "\r";
-				else result += type;
-				i += 2;
-			}
-		} else {
-			result += code[i];
-			i += 1;
+	let comments = [];
+	
+	if (sections.comm) {
+		for (let i = 0; i < sections.comm.length; i++) {
+			loadSection("comm", i);
+			comments.push(parseText());
 		}
 	}
-	return result;
+	
+	return {width, height, pixels, comments}
+	
+	
+	function loadSection(id, i) {
+		let s = sections[id];
+		if (i !== undefined) s = s[i];
+		[section, offset] = s;
+		j = 0;
+	}
+	
+	function parseString(length, allowExpr) {
+		checkEnd();
+		let startJ = j,
+			string = "";
+		while (j < section.length && string.length < length) {
+			if (allowExpr && !allowExpr.test(section[j])) throw new Error(`Unexpected character ${JSON.stringify(section[j])} at ${j + 1 + offset}`);
+			string += section[j++];
+		}
+		if (string.length < length) throw sectionEndError();
+		return string;
+	}
+	
+	function parseNumber(validate, length, digits = "0123456789") {
+		checkEnd();
+		let maxLength = length || Infinity,
+			startJ = j,
+			string = "";
+		while (j < section.length && digits.indexOf(section[j]) > -1 && string.length < maxLength) {
+			string += section[j++];
+		}
+		if (length && string.length < length) throw new Error(`Expected ${length}-digit${digits.length === 10 ? "" : " base-" + digits.length} number at character ${startJ + 1 + offset}`);
+		let num = parseInt(string, digits.length);
+		if (validate && !validate(num)) throw new Error(`Invalid number at character ${startJ + 1 + offset}`);
+		return num;
+	}
+	
+	function parseText() {
+		var result = "";
+		while (j < section.length) {
+			if (section[j] === "\\") {
+				j++;
+				let type = section[j++];
+				if (type === "u") {
+					let code = "";
+					while (j < section.length && /[0-9a-f]/.test(section[j])) {
+						code += section[j++];
+					}
+					if (section[j] === ";") j++;
+					result += String.fromCodePoint(parseInt(code, 16));
+				} else if (type === "c") {
+					if (section[j]) result += section[j++].toUpperCase();
+				} else {
+					if (type === "l") result += "/";
+					else if (type === "s") result += " ";
+					else if (type === "t") result += "\t";
+					else if (type === "n") result += "\n";
+					else if (type === "r") result += "\r";
+					else result += type || "";
+				}
+			} else {
+				result += section[j++];
+			}
+		}
+		return result;
+	}
+	
+	function checkEnd() {
+		if (j >= section.length) throw sectionEndError();
+	}
+	
+	function sectionEndError() {
+		return new Error(`Unexpected end of section after character ${j + offset}`);
+	}
+	
+	function parseSection() {
+		if (code[i] === "/") i++;
+		if (i < code.length) {
+			let name;
+			if (code[i] === "/") {
+				i++;
+				name = "";
+				while (name.length < 4 && i < code.length && code[i] !== "/") {
+					name += code[i++];
+				}
+				if (name.length < 4) {
+					if (code[i] === "/") throw slashError();
+					else throw endError();
+				}
+			}
+			let offset = i, content = "";
+			while (i < code.length && code[i] !== "/") {
+				content += code[i++];
+			}
+			return [name, content, offset];
+		} else throw endError();
+	}
+	
+	function endError() {
+		return new Error(`Unexpected end of input after character ${i}`);
+	}
+	
+	function slashError() {
+		return new Error(`Unexpected slash at character ${i + 1}`);
+	}
 }
 
